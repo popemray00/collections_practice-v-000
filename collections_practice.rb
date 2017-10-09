1d1c7c2813d14477840e8e3ca21833f5d704f3ab
Michael Pope <popemray1986@gmail.com>
2017-10-09 04:08:26 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array.sort do |a, b|
+    a[1] <=> b[2]
+  end
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each { |name| name[2]="$"}
+end
+
+def find_a(array)
+  array.find_all { |i| i.start_with? "a"}
+end
+
+def sum_array(array)
+  array.inject(:+)
+end
+
+def add_s(array)
+  array.each_with_index.collect { |element, index|
+    if index == 1
+      element
+    else
+      element + "s"
+      end
+    }
+end
